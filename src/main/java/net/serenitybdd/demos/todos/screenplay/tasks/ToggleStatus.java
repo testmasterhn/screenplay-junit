43ff2f9ced65b5953e391e4c24ad5d23b9d95ad1
khanhtran86 <khanh.tx@live.com>
2018-12-26 20:44:37 +0700

    Initial Project

---
+++ src/main/java/net/serenitybdd/demos/todos/screenplay/tasks/ToggleStatus.java
@@ -0,0 +1,23 @@
+package net.serenitybdd.demos.todos.screenplay.tasks;
+
+import static net.serenitybdd.screenplay.Tasks.instrumented;
+
+import net.serenitybdd.demos.todos.screenplay.user_interface.TodoList;
+import net.serenitybdd.screenplay.Actor;
+import net.serenitybdd.screenplay.Task;
+import net.serenitybdd.screenplay.abilities.BrowseTheWeb;
+import net.thucydides.core.annotations.Step;
+
+public class ToggleStatus implements Task {
+
+    @Override
+    @Step("{0} toggles the status of all items")
+    public <T extends Actor> void performAs(T theActor) {
+        BrowseTheWeb.as(theActor).evaluateJavascript("arguments[0].click();",
+                                                     TodoList.TOGGLE_ALL.resolveFor(theActor));
+    }
+
+    public static ToggleStatus ofAllItems() {
+        return instrumented(ToggleStatus.class);
+    }
+}
---
+++ src/test/java/net/serenitybdd/demos/todos/screenplay/features/completing_todos/ToggleAllTodos.java
@@ -0,0 +1,103 @@
+package net.serenitybdd.demos.todos.screenplay.features.completing_todos;
+
+import static net.serenitybdd.demos.todos.screenplay.model.TodoStatus.Active;
+import static net.serenitybdd.demos.todos.screenplay.model.TodoStatus.Completed;
+import static net.serenitybdd.screenplay.GivenWhenThen.givenThat;
+import static net.serenitybdd.screenplay.GivenWhenThen.seeThat;
+import static net.serenitybdd.screenplay.GivenWhenThen.then;
+import static net.serenitybdd.screenplay.GivenWhenThen.when;
+import static org.hamcrest.CoreMatchers.is;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.openqa.selenium.WebDriver;
+
+import net.serenitybdd.demos.todos.screenplay.questions.TheItemStatus;
+import net.serenitybdd.demos.todos.screenplay.questions.TheItems;
+import net.serenitybdd.demos.todos.screenplay.tasks.Start;
+import net.serenitybdd.demos.todos.screenplay.tasks.ToggleStatus;
+import net.serenitybdd.junit.runners.SerenityRunner;
+import net.serenitybdd.screenplay.Actor;
+import net.serenitybdd.screenplay.abilities.BrowseTheWeb;
+import net.thucydides.core.annotations.Managed;
+import net.thucydides.core.annotations.WithTag;
+import net.thucydides.core.annotations.WithTags;
+
+@RunWith(SerenityRunner.class)
+@WithTags({
+        @WithTag("Screenplay pattern"),
+        @WithTag("version:RELEASE-2"),
+})
+public class ToggleAllTodos {
+
+    @Managed
+    private WebDriver hisBrowser;
+    private Actor james = Actor.named("James");
+
+    @Before
+    public void jamesCanBrowseTheWeb() {
+        james.can(BrowseTheWeb.with(hisBrowser));
+    }
+
+    @Test
+    public void should_be_able_to_quickly_complete_all_todos() {
+
+        givenThat(james).wasAbleTo(Start.withATodoListContaining("Walk the dog", "Put out the garbage"));
+
+        when(james).attemptsTo(
+                ToggleStatus.ofAllItems()
+        );
+
+        then(james).should(
+                seeThat(TheItemStatus.forTheItemCalled("Walk the dog"), is(Completed)),
+                seeThat(TheItemStatus.forTheItemCalled("Put out the garbage"), is(Completed))
+        );
+    }
+
+    @Test
+    public void should_be_able_to_toggle_status_of_all_todos() {
+
+        givenThat(james).wasAbleTo(Start.withATodoListContaining("Walk the dog", "Put out the garbage"));
+
+        when(james).attemptsTo(
+                ToggleStatus.ofAllItems(),
+                ToggleStatus.ofAllItems()
+        );
+
+        then(james).should(
+                seeThat(TheItemStatus.forTheItemCalled("Walk the dog"), is(Active)),
+                seeThat(TheItemStatus.forTheItemCalled("Put out the garbage"), is(Active))
+        );
+    }
+
+
+    @Test
+    public void should_see_that_there_are_zero_items_todo_when_all_are_toggled_complete() {
+
+        givenThat(james).wasAbleTo(Start.withATodoListContaining("Walk the dog", "Put out the garbage"));
+
+        when(james).attemptsTo(
+                ToggleStatus.ofAllItems()
+        );
+
+        then(james).should(
+                seeThat(TheItems.leftCount(), is(0))
+        );
+    }
+
+    @Test
+    public void should_see_how_many_items_todo_when_all_are_toggled_to_incomplete() {
+
+        givenThat(james).wasAbleTo(Start.withATodoListContaining("Walk the dog", "Put out the garbage"));
+
+        when(james).attemptsTo(
+                ToggleStatus.ofAllItems(),
+                ToggleStatus.ofAllItems()
+        );
+
+        then(james).should(
+                seeThat(TheItems.leftCount(), is(2))
+        );
+    }
+}
